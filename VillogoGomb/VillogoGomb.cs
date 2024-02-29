734c58e41f2f25c3e476e7d67f36a7b65f5b783b
barnass1 <barni.medve@icloud.com>
2024-02-29 12:19:57 +0100

    gyak3

---
+++ VillogoGomb/VillogoGomb.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VillogoGomb
+{
+    internal class VillogoGomb : Button
+    {
+        System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
+        public VillogoGomb() 
+        {
+            BackColor = Color.Yellow;
+            MouseEnter += VillogoGomb_MouseEnter;
+            // MouseLeave += VillogoGomb_MouseLeave;
+            timer.Tick += Timer_Tick;
+        }
+
+        private void Timer_Tick(object? sender, EventArgs e)
+        {
+            BackColor = SystemColors.ButtonFace;
+            timer.Stop();
+        }
+
+        private void VillogoGomb_MouseLeave(object? sender, EventArgs e)
+        {
+            BackColor = SystemColors.ButtonFace;
+        }
+
+        private void VillogoGomb_MouseEnter(object? sender, EventArgs e)
+        {
+            BackColor = Color.Fuchsia;
+            timer.Interval = 1000;
+            timer.Start();
+        }
+    }
+}
